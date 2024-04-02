3764b0a3c1827f9acd105e889a6d4c71bc28edd1
Sarolta <115449000+saroltah@users.noreply.github.com>
2024-04-02 22:01:24 +0200

    add class to comments.models.py

--- comments/models.py
+++ comments/models.py
@@ -1,4 +1,9 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
 
+class Comments(models.Model):
+  author = models.OneToOneField(profiles.username, on_delete=models.CASCADE)
+  content	= models.TextField()		
+  created_on	= models.DateTimeField(auto_now_add=True)
