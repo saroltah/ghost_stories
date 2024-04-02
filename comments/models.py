fe297dbfe9a34bfdf879e2a716087deb52e74924
Sarolta <115449000+saroltah@users.noreply.github.com>
2024-04-02 22:59:03 +0200

    corrected model errors according to the terminal

--- comments/models.py
+++ comments/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.utils import timezone
+from profiles.models import Writer, Editor
 
 # Create your models here.
 
 class Comments(models.Model):
-  author = models.OneToOneField(profiles.username, on_delete=models.CASCADE)
+  author = models.OneToOneField(Writer, on_delete=models.CASCADE)
   content	= models.TextField()		
   created_on	= models.DateTimeField(auto_now_add=True)
--- stories/models.py
+++ stories/models.py
@@ -1,5 +1,14 @@
 from django.db import models
 from django.utils import timezone
+from profiles.models import Writer
 
 # Create your models here.
 
+class Xyz(models.Model):
+  author = models.OneToOneField(Writer, on_delete=models.CASCADE)
+  title	= models.CharField(max_length=100)	
+  teaser = models.TextField(max_length=500)
+  story	= models.TextField()
+  image	= models.ImageField(blank=True, upload_to=None, height_field=None, width_field=None)	
+  created_on	= models.DateTimeField(auto_now_add=True)
+  edited = models.DateTimeField	(auto_now=True)
