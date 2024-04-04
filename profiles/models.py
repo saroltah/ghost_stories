fdcd0c8ed4623e63de43751f0b56cffd3bb34a78
Sarolta <115449000+saroltah@users.noreply.github.com>
2024-04-04 17:48:04 +0200

    add method to profiles.model

--- profiles/models.py
+++ profiles/models.py
@@ -11,7 +11,9 @@ class Writer(models.Model):
   photo	= models.ImageField(blank=True, upload_to=None, height_field=None, width_field=None)
   about_me = models.TextField(blank=True, max_length=300)	
   resume = models.FileField(blank=True)
-		
+
+  def __str__(self):
+    return f"Writer: {self.username} : {self.name}"
 
 class Editor(models.Model):
   name = models.CharField(max_length=50)
@@ -21,4 +23,7 @@ class Editor(models.Model):
   photo	= models.ImageField(blank=True, upload_to=None, height_field=None, width_field=None)	
   company	= models.CharField(max_length=100)
   about_me = models.TextField(blank=True, max_length=300)
-  looking_for	= models.TextField(blank=True)	
+  looking_for	= models.TextField(blank=True)
+
+  def __str__(self):
+    return f"Editor: {self.name}"
