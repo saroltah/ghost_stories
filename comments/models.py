53a68bda5b45b2144ba26d4a9553897d0001e21b
Sarolta <115449000+saroltah@users.noreply.github.com>
2024-04-04 17:47:44 +0200

    add method to comments.model

--- comments/models.py
+++ comments/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from profiles.models import Writer, Editor
+from stories.models import Story
 
 # Create your models here.
 
@@ -8,3 +9,10 @@ class Comment(models.Model):
   author = models.OneToOneField(Writer, on_delete=models.CASCADE)
   content	= models.TextField()		
   created_on	= models.DateTimeField(auto_now_add=True)
+  story = models.ManyToManyField(Story)
+
+  class Meta:
+    ordering = ["-created_on"]
+
+  def __str__(self):
+    return f"{self.author} says: {self.content}."
