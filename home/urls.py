62f93ed16443fb77480e954cb6286ed138c72b24
Sarolta <115449000+saroltah@users.noreply.github.com>
2024-04-14 17:13:45 +0200

    add basic text and styling to about page

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.Index.as_view(), name='index'),
-    path('about/', views.Index.as_view(), name='about'),
+    path('about/', views.About.as_view(), name='about'),
     path('terms/', views.Index.as_view(), name='terms'),
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,6 @@ from django.views.generic import TemplateView
 
 class Index(TemplateView):
     template_name = "index.html"
+
+class About(TemplateView):
+    template_name = "about.html"
