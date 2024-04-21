c91f91d20dc5ca431de483e607c61c1a66d674e3
Sarolta <115449000+saroltah@users.noreply.github.com>
2024-04-21 15:11:11 +0200

    add filter to comment view, so we see only related comments

--- stories/views.py
+++ stories/views.py
@@ -14,7 +14,7 @@ class OneStory(generic.DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['comments'] = Comment.objects.all()
+        context['comments'] = Comment.objects.filter(commented_story=self.object)
         return context
     
 class AddStory(generic.CreateView):
