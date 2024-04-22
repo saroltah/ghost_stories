b415cdc09227047cca17e1b30629a1cdb37877ba
Sarolta <115449000+saroltah@users.noreply.github.com>
2024-04-22 21:20:18 +0200

    install summernote

--- ghost_stories/urls.py
+++ ghost_stories/urls.py
@@ -17,12 +17,18 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from home import views as index_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('', include('home.urls'), name='homepage-urls'),
     path('admin/', admin.site.urls),
+    path('summernote/', include('django_summernote.urls')),
     path('accounts/', include('allauth.urls')),
     path('stories/', include('stories.urls'), name='stories-urls'),
     path('users/', include('profiles.urls'), name='user-urls'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- stories/admin.py
+++ stories/admin.py
@@ -1,7 +1,14 @@
 from django.contrib import admin
 from .models import Story, Comment, Like
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
-admin.site.register(Story)
+@admin.register(Story)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title',)
+    search_fields = ['title']
+    summernote_fields = ('story_text',)
+
 admin.site.register(Comment)
 admin.site.register(Like)
